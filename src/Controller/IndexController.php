30107da3389c0e9a9496e64a621d3a15e0b42e9c
TeddyH08 <teddy.hemonet@gmail.com>
2022-05-21 14:52:58 +0200

    test

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+    namespace App\Controller;
+
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
+
+    class IndexController extends AbstractController {
+        /**
+         * @Route("/index", name="index")
+         * @return Response
+         */
+        public function index(): Response {
+            return $this->render('page/index.html.twig');
+        }
+    }
